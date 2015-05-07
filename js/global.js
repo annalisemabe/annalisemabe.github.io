ddcb3b5989205087559006149bf005aa8a5f7526
Travis Benton <tbenton@spredfast.com>
2015-05-07 08:47:31 -0500

    content

--- js/global.js
+++ js/global.js
@@ -50,7 +50,7 @@ var View = Backbone.View.extend({
       	duration: this.duration
 	    });
 
-    this.$pageRight.find('h1, p')
+    this.$pageRight.find('img, h1, h2, p')
     	.delay(this.duration)
     	.velocity('transition.slideUpIn');
   },
